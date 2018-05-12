748249c1dd413e112af6ce3045fb2655032a1198
haitchex <canti89@gmail.com>
2018-05-12 16:51:14 -0500

    177 Switching Between Pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import './Blog.css'
 import Posts from './Posts/Posts'
+import NewPost from './NewPost/NewPost'
 import {Route} from 'react-router-dom'
 
 class Blog extends Component {
@@ -16,16 +17,7 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts} />
-                {/*
-                <Route
-                    path="/"
-                    exact
-                    render={() => (<h1>Home</h1>)} />
-                <Route
-                    path="/new-post"
-                    exact
-                    render={() => (<h1>new posts</h1>)} />
-                */}
+                <Route path="/new-post" exact component={NewPost} />
             </div>
         )
     }
