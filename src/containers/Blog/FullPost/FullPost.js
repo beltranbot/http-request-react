a2df4ee0ba529dbe0ce89308db513fc47bc58be3
haitchex <canti89@gmail.com>
2018-05-12 21:16:46 -0500

    185 Extracting Route Parameters

--- src/containers/Blog/FullPost/FullPost.js
+++ src/containers/Blog/FullPost/FullPost.js
@@ -8,29 +8,36 @@ class FullPost extends Component {
         loadedPost: null
     }
 
-    async componentDidUpdate () {
-        if (!this.props.id) return
+    async componentDidMount () {
+        const post_id = this.props.match.params.id
+        if (!post_id) return
         if (this.state.loadedPost
             && (!this.state.loadedPost
-                || this.state.loadedPost.id === this.props.id)) return
+                || this.state.loadedPost.id === post_id)) return
         
         const response = await axios.get(
-            'https://jsonplaceholder.typicode.com/posts/' + this.props.id
+            'https://jsonplaceholder.typicode.com/posts/' + post_id
         )
         this.setState({loadedPost: response.data})
     }
 
     deletePostHandler = async () => {
+        const post_id = this.props.match.params.id
         const response = await axios.delete(
-            'https://jsonplaceholder.typicode.com/posts/' + this.props.id
+            'https://jsonplaceholder.typicode.com/posts/' + post_id
         )
         console.log(response)
     }
 
     render () {
-        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>
+        const post_id = this.props.match.params.id
+        let post = (
+            <p style={{textAlign: 'center'}}>
+                Please select a Post!
+            </p>
+        )
 
-        if (this.props.id) {
+        if (post_id) {
             post = <p style={{textAlign: 'center'}}>Loading...</p>
         }
 
