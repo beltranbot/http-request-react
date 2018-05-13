99aac0b60526540990706bf4ddb00049e1a490ad
haitchex <canti89@gmail.com>
2018-05-12 21:23:15 -0500

    188 Navigating Programmatically

--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import axios from '../../../axios'
 import Post from '../../../components/Post/Post'
-import { Link } from 'react-router-dom'
+// import { Link } from 'react-router-dom'
 
 import './Post.css'
 
@@ -12,7 +12,10 @@ class Posts extends Component {
     }
 
     postSelectedHandler = (id) => {
-        this.setState({ selectedPostId: id })
+        // this.setState({ selectedPostId: id })
+        this.props.history.push({
+            pathname: '/' + id
+        })
     }
 
     async componentDidMount() {
@@ -33,14 +36,15 @@ class Posts extends Component {
         if (!this.state.error) {
             posts = this.state.posts.map(post => {
                 return (
-                    <Link to={'/' + post.id} key={post.id}>
-                        <Post                            
+                    // <Link to={'/' + post.id} key={post.id}>
+                        <Post
+                            key={post.id}
                             title={post.title}
                             author={post.author}
                             clicked={() => (
                                 this.postSelectedHandler(post.id)
                             )} />
-                    </Link>
+                    // </Link>
                 )
             })
         }
